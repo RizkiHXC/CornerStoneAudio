388abc8fadf96268c6e90bdce4d659647efde4e2
Rizki Calame <rizki@rizkicalame.com>
2014-11-05 18:15:36 +0100

    Mail finished

--- php/mail.php
+++ php/mail.php
@@ -1,16 +1,18 @@
 <?php
 	// Data
-	$selectedServices  = 'None';
-	if(isset($_POST['services']) && is_array($_POST['services']) && count($_POST['services']) > 0){
-	    $selectedServices = implode(', ', $_POST['services']);
+	$selectedServices = "";
+
+	foreach ($_POST['services'] as $value) {
+    	$selectedServices .= "\r\n".$value;
 	}
 
 	// Mail specifics
 	$to 			= "rizki@rizkicalame.com";
-	$subject 		= "Webform Inquiry Daan is een poepie";
+	$subject 		= "Webform Inquiry";
 	$linebreak		= "\r\n\r\n";
-	$message 		= "Full name: ".$_POST["full-name"].$linebreak."Band name: ".$_POST["band-name"].$linebreak."Email: ".$_POST["email"].$linebreak."What are you looking for?: ".$$selectedServices.$linebreak."Number of songs: ".$_POST["nr-songs"].$linebreak."Explain what you need (if you selected other, please clarify)".$_POST["explanation"].$linebreak."Budget: ".$_POST["budget"]."Dates: ".$_POST["date-from"]." to ".$_POST["date_to"]."Daan, je bent een poepie";
+	$message 		= "Full name: ".$_POST["full-name"].$linebreak."Band name: ".$_POST["band-name"].$linebreak."Email: ".$_POST["email"].$linebreak."What are you looking for?: ".$selectedServices.$linebreak."Number of songs: ".$_POST["nr-songs"].$linebreak."Explain what you need (if you selected other, please clarify): ".$_POST["explanation"].$linebreak."Budget: ".$_POST["budget"].$linebreak."Dates: ".$_POST["date-from"]." to ".$_POST["date-to"];
 
 	mail($to, $subject, $message);
+
+	header('Location: http://www.cornerstoneaudio.nl');   
 ?>
-<!-- "Full name: ".$_POST["full-name"].$linebreak. -->
